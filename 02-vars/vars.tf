84653fc56d9988b0a1e6b059f3f4e854d9dd5303
rganjare <rahul.ganjare@gmail.com>
2022-06-16 18:55:30 +0530

    updated

--- 02-vars/vars.tf
+++ 02-vars/vars.tf
@@ -5,3 +5,64 @@ variable "sample" {
 output "variable" {
     value = var.sample
 }
+
+## Data Types -- String, Number & Boolean
+
+## String Data Type
+variable "string" {
+    default = "Hello World"
+}
+
+output "string" {
+    value = var.string
+}
+
+## Number Data Type
+
+variable "number" {
+    type = number
+    description = "(optional) describe your variable"
+    default = 100
+}
+
+output "number" {
+    value = var.number
+}
+## Boolean Data Type -- list & Map
+variable "boolean" {
+    type = boolean
+    description = "(optional) describe your variable"
+    default = true
+}
+
+output "boolean" {
+    value = var.boolean
+}
+
+## Variable types
+variable "list" {
+    type = list()
+    description = "(optional) describe your variable"
+    default = [
+       "I am in list",
+        100,
+        true
+    ]
+    }
+
+output "list" {
+    value = var.list[0]
+}
+variable "map" {
+    type = map()
+    description = "(optional) describe your variable"
+    default = {
+        key1 = "String"
+        key2 = 100
+        key3 = true
+    }
+}
+
+output "map" {
+    value = var.map[key3]
+}
