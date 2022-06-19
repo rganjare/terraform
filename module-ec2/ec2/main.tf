fd688097c73c93543a72ccd35fe5fd3e146f0c1e
rganjare <rahul.ganjare@gmail.com>
2022-06-19 08:45:03 +0530

    EC2-instance with module

--- module-ec2/ec2/main.tf
+++ module-ec2/ec2/main.tf
@@ -1,6 +1,5 @@
 resource "aws_instance" "sample" {
   ami                    = "ami-0bb6af715826253bf"
-#   ami                    = data.aws_ami.example.image_id
   instance_type          = "t3.micro"
   vpc_security_group_ids = [var.secgrp]
 }
